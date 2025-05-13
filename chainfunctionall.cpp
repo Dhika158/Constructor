ff35267066cba06f55325933df8e9bd62d0a3dc3
Dhika158 <andhikanurjamil7@gmail.com>
2025-05-13 16:43:45 +0700

    chani function

--- chainfunctionall.cpp
+++ chainfunctionall.cpp
@@ -3,16 +3,19 @@ using namespace std;
 class buku{
     string judul;
 
-    public: 
-    string setget(string judul){
+    public:
+    buku setjudul(string judul){
         this->judul = judul;
+        return *this;
+    }
+    string getJudul(){
         return this->judul;
     }
 };
 
 int main()
 {
-    buku bukunya;
-    cout<<bukunya.setget("Matematika");
+    buku bukunya:
+    cout<<bukunya.setJudul("Matematika").getJudul();
     return 0;
 }
